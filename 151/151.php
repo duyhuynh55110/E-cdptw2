6be7868696be7c4548d98253b643dfc8ae136c87
dohongvy <vykhtn1998@gmail.com>
2019-10-19 15:46:06 +0700

    nhánh Vy1

---
+++ 151/151.php
@@ -0,0 +1,42 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/151.less', 'css/151.css');
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Module 151</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="<?php echo $url_path ?>/css/font-awesome-v4.6.3.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/css/bootstrap-v3.3.6.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/css/151.css" rel="stylesheet" type="text/css" />
+
+    <script src="<?php echo $url_path ?>/js/jquery-v2.2.4.min.js"></script>
+    <script src="<?php echo $url_path ?>/js/bootstrap-v3.3.2.min.js"></script>
+ 
+   
+</head>
+
+<body>
+    <?php include $dir_block.'/151-content.php'; ?>
+
+</body>
+
+</html>
---
+++ 163/163.php
@@ -0,0 +1,48 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/163.less', 'css/163.css');
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Module 160</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="<?php echo $url_path ?>/css/163.css" rel="stylesheet" />
+
+
+    <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
+    <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo $url_path ?>/js/chart.js"></script>
+    <script src="<?php echo $url_path ?>/js/script-gauge.js"></script>
+    <script src="<?php echo $url_path ?>/js/weather-icons.js"></script>
+    <script src="<?php echo $url_path ?>/js/init-script.js"></script>
+    <script src="<?php echo $url_path ?>/js/jquery.jqGauges.min.js"></script>
+    <script src="<?php echo $url_path ?>/js/highcharts.js"></script>
+    <script src="<?php echo $url_path ?>/js/collapse-close.js"></script>
+</head>
+
+<body>
+<?php include $dir_block.'/163-content.php'; ?>
+</body>
+
+</html>
